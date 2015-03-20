1c2b5455cc5cacad3ff16225536c596275ba2987
Mishal Islam <mishal.islam01@gmail.com>
2015-03-20 10:04:23 +0000

    refactored

--- Answer.js
+++ Answer.js
@@ -1,47 +1,64 @@
 var $ = function (selector) {
-  var elements = [];
-  var bodyElements = document.body.children;
-  var splitArray = selector.split(/(?=\.)|(?=#)/)
-  for(var position = 0; position < splitArray.length; position++){
-    var tempArray = []
-    var element = splitArray[position]
-    if(checkIfClass(element)){
-      element = element.substring(1)
-      for(var pos = 0; pos < bodyElements.length; pos++){
-        if(bodyElements[pos].className.indexOf(element) > -1){
-          tempArray.push(bodyElements[pos])
-        }
-      }
-      bodyElements = tempArray
-    } else if(checkIfId(element)){
-      element = element.substring(1)
-      for(var pos = 0; pos < bodyElements.length; pos++){
-        if(bodyElements[pos].id === element){
-          tempArray.push(bodyElements[pos])
-        }
-      }
-      bodyElements = tempArray
-    } else {
-      for(var pos = 0; pos < bodyElements.length; pos++){
-        if(bodyElements[pos].tagName === element.toUpperCase()){
-          tempArray.push(bodyElements[pos])
-        }
-      }
-      bodyElements = tempArray;
-    }
-  };
-
-  for(var pos = 0; pos < bodyElements.length; pos++){
-    elements.push(bodyElements[pos])
-  }
-
-  return elements;
+  var htmlSelector = new JavascriptSelector(selector)
+  return htmlSelector.returnElements();
 }
 
-var checkIfClass = function(element){
-  return element[0] === '.'
+var JavascriptSelector = function(selector){
+  this.bodyElements = document.body.children
+  this.elements = []
+  this.splitSelector = selector.split(/(?=\.)|(?=#)/)
 }
 
-var checkIfId = function(element){
-  return element[0] === '#'
-}
+JavascriptSelector.prototype.returnElements = function() {
+  for(var position = 0; position < this.splitSelector.length; position++)
+    this.forEachSelector(position)
+  this.addToElementsArray()
+  return this.elements
+};
+
+JavascriptSelector.prototype.forEachSelector = function(position) {
+  var tempArray = []
+  var element = this.splitSelector[position]
+  this.checkWhatSelectorIs(element, tempArray)
+};
+
+JavascriptSelector.prototype.selectorIsClass = function(element, tempArray) {
+  this.checkBodyElements(element, tempArray, this.checkAnElementsClass)
+};
+
+JavascriptSelector.prototype.selectorIsId = function(element, tempArray) {
+  this.checkBodyElements(element, tempArray, this.checkAnElementsId)
+};
+
+JavascriptSelector.prototype.selectorIsTagName = function(element, tempArray) {
+  this.checkBodyElements(element, tempArray, this.checkAnElementsTagName)
+};
+
+JavascriptSelector.prototype.checkAnElementsId = function(element, bodyElement) {
+  return bodyElement.id === element
+};
+
+JavascriptSelector.prototype.checkAnElementsClass = function(element, bodyElement) {
+  return bodyElement.className.indexOf(element) > -1
+};
+
+JavascriptSelector.prototype.checkAnElementsTagName = function(element, bodyElement) {
+  return bodyElement.tagName === element.toUpperCase()
+};
+
+JavascriptSelector.prototype.checkBodyElements = function(element, tempArray, ifStatement) {
+  for(var pos = 0; pos < this.bodyElements.length; pos++)
+    if(ifStatement(element, this.bodyElements[pos])) tempArray.push(this.bodyElements[pos])
+  this.bodyElements = tempArray;
+};
+
+JavascriptSelector.prototype.addToElementsArray = function() {
+  for(var pos = 0; pos < this.bodyElements.length; pos++)
+    this.elements.push(this.bodyElements[pos])
+};
+
+JavascriptSelector.prototype.checkWhatSelectorIs = function(element, tempArray) {
+  if(element[0] === '.') this.selectorIsClass(element.substring(1), tempArray)
+  if(element[0] === '#') this.selectorIsId(element.substring(1), tempArray)
+  else this.selectorIsTagName(element, tempArray)
+};
